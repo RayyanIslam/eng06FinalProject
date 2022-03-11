04e6979bf50da0d33e65357c32432bd8b6ced049
DaQuavis17 <100720107+DaQuavis17@users.noreply.github.com>
2022-03-10 16:31:46 -0800

    Add files via upload

---
+++ Cards.m
@@ -0,0 +1,68 @@
+
+filepath = "/Users/matthewwang/Desktop/FinalPro/CardDeck/";
+background = [filepath, 'PokerTable.png'];
+
+Aspade = [filepath, 'aSpades.png'];
+Aheart = [filepath, 'aHearts.png'];
+Aclub = [filepath, 'aClubs.png'];
+Adiamond = [filepath, 'aDiamonds.png'];
+
+twospade = [filepath, '2Spades.png'];
+twoheart = [filepath, '2Hearts.png'];
+twoclub = [filepath, '2Clubs.png'];
+twodiamond = [filepath, '2Diamonds.png'];
+
+threespade = [filepath, '3Spades.png'];
+threeheart = [filepath, '3Hearts.png'];
+threeclub = [filepath, '3Clubs.png'];
+threediamond = [filepath, '3Diamonds.png'];
+
+fourspade = [filepath, '4Spades.png'];
+fourheart = [filepath, '4Hearts.png'];
+fourclub = [filepath, '4Clubs.png'];
+fourdiamond = [filepath, '4Diamonds.png'];
+
+fivespade = [filepath, '5Spades.png'];
+fiveheart = [filepath, '5Hearts.png'];
+fiveclub = [filepath, '5Clubs.png']; 
+fivediamond = [filepath, '5Diamonds.png'];
+
+sixspade = [filepath, '6Spades.png'];
+sixheart = [filepath, '6Hearts.png'];
+sixclub = [filepath, '6Clubs.png'];
+sixdiamond = [filepath, '6Diamonds.png'];
+
+sevenspade = [filepath, '7Spades.png'];
+sevenheart = [filepath, '7Hearts.png'];
+sevenclub = [filepath, '7Clubs.png'];
+sevendiamond = [filepath, '7Diamonds.png'];
+
+eightspade = [filepath, '8Spades.png'];
+eightheart = [filepath, '8Hearts.png'];
+eightclub = [filepath, '8Clubs.png'];
+eightdiamond = [filepath, '8Diamonds.png'];
+
+ninespade = [filepath, '9Spades.png'];
+nineheart = [filepath, '9Hearts.png'];
+nineclub = [filepath, '9Clubs.png'];
+ninediamond = [filepath, '9Diamonds.png'];
+
+tenspade = [filepath, '10Spades.png'];
+tenheart = [filepath, '10Hearts.png'];
+tenclub = [filepath, '10Clubs.png'];
+tendiamond = [filepath, '10Diamonds.png'];
+
+Jspade = [filepath, 'jSpades.png'];
+Jheart = [filepath, 'jHearts.png'];
+Jclub = [filepath, 'jClubs.png'];
+Jdiamond = [filepath, 'jDiamonds.png'];
+
+Qspade = [filepath, 'qSpades.png'];
+Qheart = [filepath, 'qHearts.png'];
+Qclub = [filepath, 'qClubs.png'];
+Qdiamond = [filepath, 'qDiamonds.png'];
+
+Kspade = [filepath, 'kSpades.png'];
+Kheart = [filepath, 'kHearts.png'];
+Kclub = [filepath, 'kClubs.png'];
+Kdiamond = [filepath, 'kDiamonds.png'];
